7e5dc4bd0e16f265a72445f6f73326c5cc6abf5f
Daniel Moran <daniel.moran@tanium.com>
2016-12-14 15:54:01 -0600

    fixing hit and node js errors

--- index.js
+++ index.js
@@ -1,77 +1,72 @@
-'use strict'
+'use strict';
 
-const got = require('got')
-const https = require('https')
-const { parse } = require('url')
+const got = require('got');
+const https = require('https');
+const url = require('url');
 
 const defaults = {
-  username: '',
-  password: '',
-  port: 8443,
-  url: 'https://192.168.0.1',
-  site: 'default',
-  ignoreSsl: false
+    username: '',
+    password: '',
+    port: 8443,
+    url: 'https://192.168.0.1',
+    site: 'default',
+    ignoreSsl: false
+};
+
+function unifi (inConfig) {
+    const config = Object.assign(defaults, inConfig);
+    const baseUrl = `${config.url}:${config.port}`;
+    const hostname = url.parse(baseUrl).hostname;
+
+    let agent;
+
+    const loginOptions = {
+        method: 'POST',
+        body: JSON.stringify({
+            username: config.username,
+            password: config.password
+        })
+    };
+
+    if (config.ignoreSsl) {
+        agent = new https.Agent({
+        host: hostname,
+        port: config.port,
+        path: '/',
+        rejectUnauthorized: false
+        });
+        loginOptions.agent = agent;
+    }
+
+    console.log(`${baseUrl}/api/login`);
+
+    const getSessionCookie = () => got(`${baseUrl}/api/login`, loginOptions).then(result => {
+        const cookie = result.headers['set-cookie'];
+        if (!cookie) {
+            throw new Error('Invalid Login Cookie');
+        }
+        return cookie;
+    });
+
+    return getSessionCookie().then(cookie => {
+        const options = {
+            headers: { cookie },
+            json: true
+        };
+
+        if (config.ignoreSsl) {
+            options.agent = agent;
+        }
+
+        const get = (url) =>
+            got(`${baseUrl}/api/s/${config.site}/${url}`, options).then(result => result.body.data);
+
+        return {
+            getAccessPoints: () => get('stat/device'),
+            getClients: () => get('stat/sta'),
+            get: get
+        };
+    });
 }
 
-function unifi (config = {}) {
-  const {
-    username,
-    password,
-    port,
-    url,
-    site,
-    ignoreSsl
-  } = Object.assign({}, defaults, config)
-  const baseUrl = `${url}:${port}`
-  const { hostname } = parse(baseUrl)
-
-  let agent
-
-  const loginOptions = {
-    method: 'POST',
-    body: JSON.stringify({
-      username,
-      password
-    })
-  }
-
-  if (ignoreSsl) {
-    agent = new https.Agent({
-      host: hostname,
-      port,
-      path: '/',
-      rejectUnauthorized: false
-    })
-    loginOptions.agent = agent
-  }
-
-  console.log(`${baseUrl}/api/login`)
-
-  const getSessionCookie = () => got(`${baseUrl}/api/login`, loginOptions).then(result => {
-    const cookie = result.headers['set-cookie']
-    if (!cookie) throw new Error('Invalid Login Cookie')
-    return cookie
-  })
-
-  return getSessionCookie().then(cookie => {
-    const options = {
-      headers: { cookie },
-      json: true
-    }
-
-    if (ignoreSsl) {
-      options.agent = agent
-    }
-
-    const get = (url = '') =>
-      got(`${baseUrl}/api/s/${site}/${url}`, options).then(result => result.body.data)
-
-    return {
-      getAccessPoints: () => get('stat/device'),
-      getClients: () => get('stat/sta'),
-      get
-    }
-  })
-}
-
-module.exports = unifi
+module.exports = unifi;
