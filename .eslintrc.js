a1c6c9aee7447a7d446bfaaf28ef046e8c07ff93
Daniel Moran <daniel.moran@tanium.com>
2016-12-14 16:21:13 -0600

    fixing hit issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,26 @@
+'use strict';
+
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
+    env: {
+        es6: true,
+        node: true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
+    extends: 'eslint:recommended',
+    parserOptions: {
+        sourceType: 'module'
     },
-    "rules": {
-        "indent": [
-            "error",
+    rules: {
+        indent: [
+            'error',
             4
         ],
-        "linebreak-style": [
-            "error",
-            "windows"
+        quotes: [
+            'error',
+            'single'
         ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
+        semi: [
+            'error',
+            'always'
         ]
     }
 };
